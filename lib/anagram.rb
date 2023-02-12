decce5e0c48521a19064410cd86715081bb8e318
nthokiesther09 <nthokiesther09@gmail.com>
2023-02-12 19:03:55 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.filter do |word|
+            word.chars.sort == @word.chars.sort
+        end
+    end
+end
+
